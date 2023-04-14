c6c78144abecaed46a0bf3402752f47122c1194b
Syaguero <Buitiensy16@gmail.com>
2023-04-14 12:09:45 +0700

    add delete edit search book

--- LibraryManagementSystem/services/IBookRepository.cs
+++ LibraryManagementSystem/services/IBookRepository.cs
@@ -11,19 +11,14 @@ namespace LibraryManagementSystem.services
     {
         Task<List<books>> Get();
 
-        //Task<List<Books>> Get();
-        //Task<Books> GetById(int id);
-
-        //Task<IEnumerable<books>> Get();
-        //Task<books> GetById(int id);
+        
         Task<Books> CreateBook(Books books);
         Task<object> EditBook(int id, PutBooks books);
         Task Delete(int id);
         Task<IQueryable<books>> GetSearchBook(string title, string author);
 
 
-        //Task Update(PutBooks PutBooks);
-        //Task Delete(int id);
+        
 
     }
 
