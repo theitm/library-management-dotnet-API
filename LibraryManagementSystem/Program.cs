78e596798b52b25fa9fdecc4114411d1ff895100
Syaguero <Buitiensy16@gmail.com>
2023-04-20 15:10:44 +0700

    Sy_create_API-searchUser

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
